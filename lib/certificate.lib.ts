f4feaa9d6d6848ae2596ae474480ba8ff0dac6ac
Bipul Hf <bipulhf@gmail.com>
2024-08-07 02:35:32 +0600

    AD

--- lib/certificate.lib.ts
+++ lib/certificate.lib.ts
@@ -1,7 +1,7 @@
 import fs from "fs-extra";
 import hbs from "handlebars";
 import path from "path";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 
 export const compile_hbs_template = async ({
   template_name,
@@ -38,7 +38,9 @@ export const generate_certificate = async ({
     // Generate certificate logic here
     const project_root_path = path.resolve();
 
-    const browser = await puppeteer.connect({ browserWSEndpoint: process.env.BROWSER_WS_ENDPOINT });
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: process.env.BROWSER_WS_ENDPOINT,
+    });
     const page = await browser.newPage();
 
     // setting content
